d298a911506460abf792517d8e93a4caee051a24
thegunner686 <gunn686@gmail.com>
2017-11-11 16:44:14 -0800

    kk

--- scripts/screen/main.js
+++ scripts/screen/main.js
@@ -1,6 +1,11 @@
-let canvas = document.getElementById("canvas"),
-    ctx = canvas.getContext("2d"),
-    width = canvas.width = window.innerWidth,
-    height = canvas.height = window.innerHeight;
 
-ctx.fillRect(0, 0, width, height);
+
+
+function init() {
+  let canvas = document.getElementById("canvas"),
+      ctx = canvas.getContext("2d"),
+      width = canvas.width = window.innerWidth,
+      height = canvas.height = window.innerHeight;
+
+  ctx.fillRect(0, 0, width, height);
+}
