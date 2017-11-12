3f1d4969f8d5d0d748431438565112fbb5f87ff3
thegunner686 <gunn686@gmail.com>
2017-11-11 16:22:16 -0800

    init

---
+++ scripts/screen/main.js
@@ -0,0 +1,6 @@
+let canvas = document.getElementById("canvas"),
+    ctx = canvas.getContext("2d"),
+    width = canvas.width = window.innerWidth,
+    height = canvas.height = window.innerHeight;
+
+ctx.fillRect(0, 0, width, height);
